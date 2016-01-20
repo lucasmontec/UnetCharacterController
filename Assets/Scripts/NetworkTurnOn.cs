57bc8aac2489b0dae2c95c9a84cb37a788c834a2
lucasmontec <lucas_carvalhaes@hotmail.com>
2016-01-20 09:09:51 -0200

    Initial testing setup for the script

---
+++ Assets/Scripts/NetworkTurnOn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+/// <summary>
+/// This class is just a simple controller that enables
+/// certain client side components that need to be turned off for
+/// multiplayer.
+/// </summary>
+public class NetworkTurnOn : NetworkBehaviour {
+
+	/*
+	 * Network setup
+	 */
+	[SerializeField]
+	Camera fpsCharacterCam;
+	[SerializeField]
+	AudioListener audioListener;
+	
+	// Player network setup
+	void Start () {
+		if (isLocalPlayer) {
+			//GetComponent<WBFirstPersonController> ().enabled = true; - This is now a shared script
+			//If want to disable player to player collision, disable controller and enable only for local player
+			//GetComponent<CharacterController>().enabled = true;
+			fpsCharacterCam.enabled = true;
+			audioListener.enabled = true;
+		}
+	}
+}
